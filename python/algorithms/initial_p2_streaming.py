cfa311d0f61d6119b7dbc244fe904aa9fbec6e17
steve <smarden1@gmail.com>
2013-08-06 22:50:06 -0400

    tweaks to the p2 algorithm -- still not correct

--- python/algorithms/initial_p2_streaming.py
+++ python/algorithms/initial_p2_streaming.py
@@ -52,8 +52,11 @@ class InitialP2Streaming(object):
         return self.markers[2]
         
     def add(self, data_point):
+        data_point = float(data_point)
+        self.n    += 1
+
         if not self.full_markers:
-            self.markers.append(float(data_point))
+            self.markers.append(data_point)
 
             if len(self.markers) == 5:
                 self.markers.sort()
@@ -71,10 +74,11 @@ class InitialP2Streaming(object):
                     self.markers[i] += self.height_delta(i)
                     self.positions[i] += d
 
-            self.positions[0] = min(self.positions[0], data_point)
-            self.positions[4] = max(self.positions[4], data_point)
+            # think i am still accidentally setting the largest marker
+            self.markers[0] = min(self.markers[0], data_point)
+            self.markers[4] = max(self.markers[4], data_point)
 
-    def height_delta(self, i, d = 1.):
+    def height_delta(self, i, d = 1):
         p2 = self.parabolic_prediction(i, d)
 
         if self.markers[i] + p2 < self.markers[i-1] or self.markers[i] + p2 > self.markers[i+1]:
@@ -86,18 +90,21 @@ class InitialP2Streaming(object):
         n = self.positions
         q = self.markers
 
-        return q[i] + d * (q[i+d] - q[i]) / float(n[i+d] - n[i])
+        # this is also probably very wrong
+        try:
+            return q[i] + d * (q[i+d] - q[i]) / float(n[i+d] - n[i])
+        except ZeroDivisionError:
+            return 1
         
-    def parabolic_prediction(self, i, d = 1.):
+    def parabolic_prediction(self, i, d = 1):
         n = self.positions
         q = self.markers
-        d = float(d)
 
         # this is almost certain to be wrong
         try:
-            return d / (n[i+1] - n[i-1]) * ((n[i] - n[i-1] + d) * ((q[i+1] - q[i]) / (n[i+1] - n[i])) + (n[i+1] - n[i] - d) * ((q[i] - q[i-1]) / (n[i] - n[i-1])))
+            return float(d) / (n[i+1] - n[i-1]) * ((n[i] - n[i-1] + d) * ((q[i+1] - q[i]) / (n[i+1] - n[i])) + (n[i+1] - n[i] - d) * ((q[i] - q[i-1]) / (n[i] - n[i-1])))
         except ZeroDivisionError:
-            return 0.
+            return 1
            
     def desired_marker_positions(self):
         return map(self.desired_marker_position, range(5))
